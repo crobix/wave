68742f592d0bb11df51df3e78d1084213044be71
loic <loic@code-rhapsodie.fr>
2025-01-23 11:50:53 +0100

    add android build

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.subapp.app',
+  appName: 'wave',
+  webDir: 'dist/wave/browser',
+  server: {
+    androidScheme: 'https',
+    // url: 'http://192.168.1.40:4200',
+    // cleartext: true,
+  },
+
+  cordova: {
+    preferences: {
+      LottieFullscreen: 'true',
+      LottieHideAfterAnimationEnd: 'true',
+      LottieAnimationLocation: 'public/assets/wave.json',
+      LottieFadeOutDuration: '1000',
+    },
+  },
+};
+
+export default config;
